7fa1ca263b14944a79d7a0b45eb9f3484365d18a
M1n3G <likming14@gmail.com>
2022-10-25 22:59:52 +0800

    UI

--- resources/views/livewire/cart-component.blade.php
+++ resources/views/livewire/cart-component.blade.php
@@ -1,3 +1,81 @@
-<div>
-    {{-- Do your work, then step back. --}}
-</div>
+<head>
+    <style>
+        body {
+            font-family: 'Source Sans Pro', sans-serif;
+            font-size: 18px;
+            color: black;
+        }
+
+        .cartTitle {
+            margin-top: 30px;
+            margin-bottom: 30px;
+        }
+    </style>
+</head>
+
+<!--Main area-->
+
+<body>
+    <main id="main" class="main-site" style="margin-top: 30px;">
+        <div class="container">
+            <h2 class="text-center cartTitle">Your Cart</h2>
+        </div>
+
+        <div class="container">
+
+            <!-- MESSAGE -->
+            <div class="alert alert-info alert-dismissible fade show" id="alert" role="alert">
+                <p>There are nothing in the Cart now. <a href="/store" class="link-primary">STORE</a></p>
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+
+            <!-- CART -->
+            <form action="" method="POST">
+                <input type="hidden" name="_method" value="POST">
+
+                <table class="table shadow p-3 mb-5 bg-body rounded">
+                    <thead class="table-dark text-center">
+                        <th></th>
+                        <th>Item Img</th>
+                        <th>Item Name</th>
+                        <th>Price</th>
+                        <th>Qty</th>
+                        <th>Total Price</th>
+                        <th>Status</th>
+                        <th>Remove</th>
+                    </thead>
+                    <tbody>
+                        <tr class="text-center">
+                            <td><input type="checkbox" name="" value="AT0001" class="form-check-input" style="width: 20px; height: 20px;"></td>
+                            <td><img src="{{ asset('Img/weilinggallery.jpg') }}" alt="img" style="width: 175px; height: 135px"></td>
+                            <td class="fw-bold fs-6">Art1</td>
+                            <td class="fw-bold fs-6">MYR 2,100</td>
+                            <td>
+                                <button type="submit" formaction="" formmethod="GET" class="btn btn-danger btn-sm ms-1"><i class="fa fa-minus" aria-hidden="true"></i></button>
+                                <input type="text" value="1" style="width: 28px" disabled>
+                                <button type="submit" formaction="" formmethod="GET" class="btn btn-success btn-sm ms-1"><i class="fa fa-plus" aria-hidden="true"></i></button>
+                            </td>
+                            <td class="fw-bold fs-6">MYR 2,100</td>
+                            <td>
+                                <span class="badge bg-success px-3" style="height: 23px">AVAILABLE</span>
+                            </td>
+                            <td>
+                                <button type="submit" class="btn btn-danger" formaction="" formmethod="GET" onclick="return confirm('Delete Confirmation')"><i class="fa fa-trash" aria-hidden="true"></i></button>
+                            </td>
+                        </tr>
+
+                    </tbody>
+                </table>
+
+                <div class="row mb-4">
+                    <div class="col">
+                        <div class="text-end">
+                            <button class="btn btn-dark proceedBtn" type="submit"><span>Proceed to Checkout</span></button>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+
+    </main>
+</body>
--- resources/views/livewire/register-component.blade.php
+++ resources/views/livewire/register-component.blade.php
@@ -2,49 +2,51 @@
   <link rel="stylesheet" type="text/css" href="{{ asset('Css/register.css') }}">
 </head>
 
-<div class="container py-5 h-100">
-  <div class="row d-flex justify-content-center align-items-center h-100">
-    <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-      <div class="card bg-white text-white" style="border-radius: 1rem;" style="box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);">
-        <div class="card-body p-5 text-center">
+<main id="main" class="main-site">
+  <div class="container py-5 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+        <div class="card bg-white text-white" style="border-radius: 1rem;" style="box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);">
+          <div class="card-body p-5 text-center">
 
-          <div class="md-5 mt-md-4 pb-5">
+            <div class="md-5 mt-md-4 pb-5">
 
-            <h2 class="fw-bold mb-2 text-uppercase" style="color:black">Sign Up</h2>
-            <p class="text-black-50 mb-5">Sign up to collect art by the world’s leading artists</p>
+              <h2 class="fw-bold mb-2 text-uppercase" style="color:black">Sign Up</h2>
+              <p class="text-black-50 mb-5">Sign up to collect art by the world’s leading artists</p>
 
-            <div class="form-outline" style="margin-bottom: 45px;">
-              <input type="name" id="username" class="form-control form-control-lg" style="height:50px;" />
-              <label class="form-label">Username</label>
+              <div class="form-outline" style="margin-bottom: 45px;">
+                <input type="name" id="username" class="form-control form-control-lg" style="height:50px;" />
+                <label class="form-label">Username</label>
+              </div>
+
+              <div class="form-outline" style="margin-bottom: 45px;">
+                <input type="email" id="email" class="form-control form-control-lg" style="height:50px;" />
+                <label class="form-label">Email</label>
+              </div>
+
+              <div class="form-outline" style="margin-bottom: 45px;">
+                <input type="password" id="password" class="form-control form-control-lg" style="height:50px;" />
+                <label class="form-label">Password</label>
+              </div>
+
+              <div class="form-outline" style="margin-bottom: 45px;">
+                <input type="password" id="conPassword" class="form-control form-control-lg" style="height:50px;" />
+                <label class="form-label">Confirm Password</label>
+              </div>
+
+
+              <button class="btn registerBtn btn-dark btn-lg" style="margin-top: 30px;" type="submit">Sign Up</button>
+
             </div>
-            
-            <div class="form-outline" style="margin-bottom: 45px;">
-              <input type="email" id="email" class="form-control form-control-lg" style="height:50px;" />
-              <label class="form-label">Email</label>
-            </div>
 
-            <div class="form-outline" style="margin-bottom: 45px;">
-              <input type="password" id="password" class="form-control form-control-lg" style="height:50px;" />
-              <label class="form-label">Password</label>
+            <div>
+              <p class="mb-0 text-black">Already have an account? <a href="/login" class="text-black-50 fw-bold">Sign In</a>
+              </p>
             </div>
-
-            <div class="form-outline" style="margin-bottom: 45px;">
-              <input type="password" id="conPassword" class="form-control form-control-lg" style="height:50px;" />
-              <label class="form-label">Confirm Password</label>
-            </div>
-            
-
-            <button class="btn registerBtn btn-dark btn-lg" style="margin-top: 30px;" type="submit">Sign Up</button>
 
           </div>
-
-          <div>
-            <p class="mb-0 text-black">Already have an account? <a href="/login" class="text-black-50 fw-bold">Sign In</a>
-            </p>
-          </div>
-
         </div>
       </div>
     </div>
   </div>
-</div>
+</main>
