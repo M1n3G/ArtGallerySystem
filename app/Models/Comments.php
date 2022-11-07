d5c171ded47858a4407627c23892e97867e06593
M1n3G <likming14@gmail.com>
2022-11-07 20:06:51 +0800

    Initial Commit

--- app/Models/Comments.php
+++ app/Models/Comments.php
@@ -4,9 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
 class Comments extends Model
-{
+{ 
     public function user()
     {
         return $this->belongsTo(User::class);
